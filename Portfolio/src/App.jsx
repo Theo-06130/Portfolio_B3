a74c2fd79732a9b6aa6e7c488ad042a8a5f0a124
Theo_Cwk <theo.cerkownik@ynov.com>
2024-08-27 19:44:28 +0200

    correction alignement

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -11,7 +11,7 @@ import TypingAnimation from "@/components/magicui/typing-animation.tsx";
 function App() {
     return (
         <>
-            <main className="w-screen h-screen overflow-hidden">
+            <main className="w-screen h-screen overflow-hidden lg:mt-28">
                 <TypingAnimation text="Work In Progress"></TypingAnimation>
                 <AnimatedListDemo></AnimatedListDemo>
             </main>
