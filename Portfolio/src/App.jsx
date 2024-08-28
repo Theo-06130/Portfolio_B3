1fa19de2efad7d9d6d4e35cbf671f3ca00715cdc
Theo_Cwk <theo.cerkownik@ynov.com>
2024-08-28 11:21:44 +0200

    Changement nom onglet

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -2,8 +2,7 @@
 
 import React from 'react';
 import './App.css';
-import BlurIn from "@/components/magicui/blur-in";
-import NumberTicker from "@/components/magicui/number-ticker";
+import {Helmet} from "react-helmet";
 import {AnimatedListDemo} from "@/components/magicui/animated-list_demo.tsx";
 import TypingAnimation from "@/components/magicui/typing-animation.tsx";
 
@@ -11,6 +10,10 @@ import TypingAnimation from "@/components/magicui/typing-animation.tsx";
 function App() {
     return (
         <>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Théo Cerkownik</title>
+            </Helmet>
             <main className="w-screen h-screen overflow-hidden lg:mt-28">
                 <TypingAnimation text="Work In Progress"></TypingAnimation>
                 <AnimatedListDemo></AnimatedListDemo>
