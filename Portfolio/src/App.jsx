89fcac55de00d566de6c7e9875ba25073b8c953e
Theo_Cwk <theo.cerkownik@ynov.com>
2024-08-28 16:28:45 +0200

    début home page ( titre, proffession, effet souris)

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -5,18 +5,29 @@ import './App.css';
 import {Helmet} from "react-helmet";
 import {AnimatedListDemo} from "@/components/magicui/animated-list_demo.tsx";
 import TypingAnimation from "@/components/magicui/typing-animation.tsx";
+import HaloEffect from "@/components/ui/HaloEffect.jsx";
+import WordRotate from "@/components/magicui/word-rotate.tsx";
+import DelayedAppearance from "@/components/ui/DelayedAppearance.jsx";
 
 
 function App() {
     return (
         <>
             <Helmet>
-                <meta charSet="utf-8" />
+                <meta charSet="utf-8"/>
                 <title>Théo Cerkownik</title>
+                <link rel="stylesheet"
+                      href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"/>
             </Helmet>
+            <HaloEffect/>
+            {/*<div className="text-center w-1/5 cursor-default relative top-[150px] left-[150px]">*/}
+            {/*    <p className="text-5xl font-bold tracking-[.10em] text-transparent hover:text-gray-600 hover:opacity-50 transition-colors duration-500">La lumière UV révèle le texte caché.</p>*/}
+            {/*</div>*/}
             <main className="w-screen h-screen overflow-hidden lg:mt-28">
-                <TypingAnimation text="Work In Progress"></TypingAnimation>
-                <AnimatedListDemo></AnimatedListDemo>
+                <TypingAnimation className="text-5xl" text="Théo Cerkownik"></TypingAnimation>
+                <DelayedAppearance delay="2000">
+                <span className="flex justify-center items-center mt-4"><h2 className="text-4xl mr-2">Je suis</h2><WordRotate className="text-4xl" words={["Étudiant", "Photographe","Motard"]}/></span>
+                </DelayedAppearance>
             </main>
         </>
     );
--- Portfolio/src/components/magicui/animated-list_demo.tsx
+++ Portfolio/src/components/magicui/animated-list_demo.tsx
@@ -45,9 +45,10 @@ let notifications = [
 
 notifications = Array.from({ length: 1 }, () => notifications).flat();
 
-const Notification = ({ name, description, icon, color, time }: Item) => {
+const Notification = ({ name, description, icon, color, time, link }: Item & { link: string }) => {
     return (
-        <figure
+        <a
+            href={link}
             className={cn(
                 "relative mx-auto min-h-fit w-full max-w-[400px] cursor-pointer overflow-hidden rounded-2xl p-4",
                 // animation styles
@@ -58,45 +59,47 @@ const Notification = ({ name, description, icon, color, time }: Item) => {
                 "transform-gpu dark:bg-transparent dark:backdrop-blur-md dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
             )}
         >
-            <div className="flex flex-row items-center gap-3">
-                <div
-                    className="flex size-10 items-center justify-center rounded-2xl"
-                    style={{
-                        backgroundColor: color,
-                    }}
-                >
-                    <span className="text-lg">{icon}</span>
+            <figure>
+                <div className="flex flex-row items-center gap-3">
+                    <div
+                        className="flex size-10 items-center justify-center rounded-2xl"
+                        style={{
+                            backgroundColor: color,
+                        }}
+                    >
+                        <span className="text-lg">{icon}</span>
+                    </div>
+                    <div className="flex flex-col overflow-hidden">
+                        <figcaption className="flex flex-row items-center whitespace-pre text-lg font-medium dark:text-white ">
+                            <span className="text-sm no-underline text-gray-100 sm:text-lg">{name}</span>
+                            <span className="mx-1 text-gray-100">·</span>
+                            <span className="text-xs text-gray-200">{time}</span>
+                        </figcaption>
+                        <p className="text-sm text-gray-100 font-normal text-left dark:text-white/60">
+                            {description}
+                        </p>
+                    </div>
                 </div>
-                <div className="flex flex-col overflow-hidden">
-                    <figcaption className="flex flex-row items-center whitespace-pre text-lg font-medium dark:text-white ">
-                        <span className="text-sm sm:text-lg">{name}</span>
-                        <span className="mx-1">·</span>
-                        <span className="text-xs text-gray-200">{time}</span>
-                    </figcaption>
-                    <p className="text-sm font-normal text-left dark:text-white/60">
-                        {description}
-                    </p>
-                </div>
-            </div>
-        </figure>
+            </figure>
+        </a>
     );
 };
 
-export function AnimatedListDemo({
-                                     className,
-                                 }: {
-    className?: string;
-}) {
+export function AnimatedListDemo({ className }: { className?: string }) {
     return (
         <div
             className={cn(
-                "relative flex h-[700px] w-full flex-col overflow-hidden rounded-lg border-none bg-background",
+                "relative flex h-[700px] w-full flex-col overflow-hidden rounded-lg border-none bg-background items-center",
                 className,
             )}
         >
             <AnimatedList>
                 {notifications.map((item, idx) => (
-                    <Notification {...item} key={idx} />
+                    <Notification
+                        {...item}
+                        key={idx}
+                        link="https://google.com" // Le même lien pour chaque élément
+                    />
                 ))}
             </AnimatedList>
         </div>
