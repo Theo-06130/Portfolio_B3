f2ff268f55e1247cfe209afd87892d93e08f3b4d
Theo-06130 <theo.cerkownik@ynov.com>
2024-09-09 21:04:26 +0200

    texte blanc peut importe le theme

--- Portfolio/src/components/Zone2.jsx
+++ Portfolio/src/components/Zone2.jsx
@@ -5,7 +5,7 @@ const Zone2 = () => {
     return (
         <div
             id="zone2"
-            className="scroll-zone h-screen bg-gradient-to-b from-black to-gray-950 flex items-center justify-around absolute top-full left-0 w-full"
+            className="scroll-zone h-screen text-white bg-gradient-to-b from-black to-gray-950 flex items-center justify-around absolute top-full left-0 w-full"
         >
             <BoxRevealForma></BoxRevealForma>
 
