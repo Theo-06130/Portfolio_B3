e3b5ba774da23104fc68ffed0b4cb0a739126ff2
Theo-06130 <theo.cerkownik@ynov.com>
2024-09-09 11:59:59 +0200

    test mobile page 2

--- Portfolio/src/components/Zone2.jsx
+++ Portfolio/src/components/Zone2.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import {BoxRevealForma} from "@/components/magicui/box-reveal_formation.tsx";
+import {BoxRevealCompetences} from "@/components/magicui/box-reveal_competences.tsx";
 
 const Zone2 = () => {
     return (
         <div
             id="zone2"
-            className="scroll-zone h-screen bg-gradient-to-b from-black to-gray-950 flex items-center justify-center absolute top-full left-0 w-full"
+            className="scroll-zone h-screen bg-gradient-to-b from-black to-gray-950 flex items-center justify-around absolute top-full left-0 w-full"
         >
-            <h1 className="text-5xl text-white">Zone 2</h1>
+            <BoxRevealForma></BoxRevealForma>
+            <BoxRevealCompetences></BoxRevealCompetences>
+
         </div>
     );
 };
---
+++ Portfolio/src/components/magicui/box-reveal_formation.tsx
@@ -0,0 +1,68 @@
+import Button  from "../ui/Button.jsx";
+import BoxReveal from "@/components/magicui/box-reveal";
+import React from "react";
+
+export function BoxRevealForma() {
+    return (
+        <div className="h-full w-full max-w-[32rem] items-center justify-center overflow-hidden pt-8">
+            <BoxReveal boxColor={"#5046e6"} duration={0.5}>
+                <p className="text-[3.5rem] font-semibold">
+                    Formations<span className="text-[#5046e6]">.</span>
+                </p>
+            </BoxReveal>
+
+            <BoxReveal boxColor={"#5046e6"} duration={0.5}>
+                <h2 className="mt-[.5rem] text-[1rem]">
+                    Baccalauréat Général - {" "}
+                    <span className="text-[#5046e6]">Section Européenne Anglais</span>
+                </h2>
+            </BoxReveal>
+
+            <BoxReveal boxColor={"#5046e6"} duration={0.5}>
+                <div className="mt-[1.5rem]">
+                    <p className="text-left">
+                        -&gt; Mes spécialisations
+                        <span className="font-semibold text-[#5046e6]"> NSI ( Numériques et Sciences Informatique )</span>,
+                        <span className="font-semibold text-[#5046e6]"> Physique-Chimie</span>,
+                        <span className="font-semibold text-[#5046e6]"> Mathématiques</span>.
+                    </p>
+                    <p className="mt-[1.5rem] text-left">
+                        -&gt; Réalisations de différents projets dont
+                        <span className="font-semibold text-[#5046e6]"> AI FOR YOUTH</span> organisé par Intel
+                        .
+                         Ce projet a pour but de permettre aux étudiants de développer des compétences en IA tout en créant un impact social significatif. <br />
+                    </p>
+                </div>
+            </BoxReveal>
+            <hr className="mt-8 rounded  bg-white"/>
+            <BoxReveal boxColor={"#5046e6"} duration={0.5}>
+                <h2 className="mt-8 text-[1rem]">
+                    Bachelor informatique - {" "}
+                    <span className="text-[#5046e6]">Spécialisation Cybersécurité jusqu'au Bac+5</span>
+                </h2>
+            </BoxReveal>
+
+            <BoxReveal boxColor={"#5046e6"} duration={0.5}>
+                <div className="mt-[1.5rem]">
+                    <p className="text-left">
+                        -&gt; Mes spécialisations
+                        <span className="font-semibold text-[#5046e6]"> NSI ( Numériques et Sciences Informatique )</span>,
+                        <span className="font-semibold text-[#5046e6]"> Physique-Chimie</span>,
+                        <span className="font-semibold text-[#5046e6]"> Mathématiques</span>.
+                    </p>
+                    <p className="mt-[1.5rem] text-left">
+                        -&gt; Réalisations de différents projets dont
+                        <span className="font-semibold text-[#5046e6]"> AI FOR YOUTH</span> organisé par Intel
+                        .
+                        Ce projet a pour but de permettre aux étudiants de développer des compétences en IA tout en créant un impact social significatif. <br />
+                    </p>
+                </div>
+            </BoxReveal>
+
+            <BoxReveal boxColor={"#5046e6"} duration={0.5}>
+                <Button className="mt-[1.6rem] bg-[#5046e6]">Explore</Button>
+            </BoxReveal>
+        </div>
+    );
+}
+
