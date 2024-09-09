af15f9c658359e1d9d29161ebc6e7bfa84efca84
Theo-06130 <theo.cerkownik@ynov.com>
2024-09-09 20:44:45 +0200

    change marge et taille titre page

--- Portfolio/src/components/magicui/box-reveal_formation.tsx
+++ Portfolio/src/components/magicui/box-reveal_formation.tsx
@@ -6,27 +6,27 @@ export function BoxRevealForma() {
     return (
         <div className="h-full w-full max-w-[32rem] items-center justify-center overflow-hidden">
             <BoxReveal boxColor={"#7CD7E9"} duration={0.5}>
-                <p className="text-3xl mt-4 font-semibold md:text-[3.5rem] md:mb-4 md:mt-10 ">
+                <p className="text-2xl mt-4 font-semibold md:text-[3.5rem] md:mb-4 md:mt-10 ">
                     Formations<span className="text-[#7CD7E9]">.</span>
                 </p>
             </BoxReveal>
 
             <BoxReveal boxColor={"#7CD7E9"} duration={0.5}>
-                <h2 className="mt-[1.5rem] text-[1.2rem] text-left md:mt-[1.8rem]">
+                <h2 className="mt-[1.2rem] text-[1.2rem] text-left md:mt-[1.8rem]">
                     Baccalauréat Général - {" "}
                     <span className="text-[#7CD7E9]">Section Européenne Anglais</span>
                 </h2>
             </BoxReveal>
 
             <BoxReveal boxColor={"#7CD7E9"} duration={0.5}>
-                <div className="mt-[1.5rem] md:mt-[1.8rem]">
+                <div className="mt-[1.2rem] md:mt-[1.8rem]">
                     <p className="text-left ml-6">
                         -&gt; Mes spécialités
                         <span className="font-semibold text-[#7CD7E9]"> NSI ( Numériques et Sciences Informatique )</span>,
                         <span className="font-semibold text-[#7CD7E9]"> Physique-Chimie</span>,
                         <span className="font-semibold text-[#7CD7E9]"> Mathématiques</span>.
                     </p>
-                    <p className="mt-[1.5rem] text-left ml-6 md:mt-[1.8rem]">
+                    <p className="mt-[1.2rem] text-left ml-6 md:mt-[1.8rem]">
                         -&gt; Réalisations de différents projets dont
                         <span className="font-semibold text-[#7CD7E9]"> AI FOR YOUTH</span> organisé par Intel
                         .
@@ -43,12 +43,12 @@ export function BoxRevealForma() {
             </BoxReveal>
 
             <BoxReveal boxColor={"#7CD7E9"} duration={0.5}>
-                <div className="mt-[1.5rem] md:mt-[1.8rem]">
+                <div className="mt-[1.2rem] md:mt-[1.8rem]">
                     <p className="text-left ml-6">
                         -&gt; En spécialisation
                         <span className="font-semibold text-[#7CD7E9]"> cybersécurité jusqu'au Bac+5</span>,
                     </p>
-                    <p className="mt-[1.5rem] text-left ml-6 md:mt-[1.8rem]">
+                    <p className="mt-[1.2rem] text-left ml-6 md:mt-[1.8rem]">
                         -&gt; Réalisations de différents projets dans des domaines variés tel que
                         <span className="font-semibold text-[#7CD7E9]"> Développement web</span>, <span className="font-semibold text-[#7CD7E9]"> Développement Application</span>,<span className="font-semibold text-[#7CD7E9]"> Gestion de réseaux</span>,<span className="font-semibold text-[#7CD7E9]"> Administration Windows</span>,<span className="font-semibold text-[#7CD7E9]"> Cybersécurité</span><span className="font-semibold text-[#7CD7E9]"> et bien plus encore</span>.
                     </p>
