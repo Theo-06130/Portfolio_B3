a3b5304185d87a72a743bdb3c68269e33f2c277b
Theo-06130 <theo.cerkownik@ynov.com>
2024-09-27 20:53:52 +0200

    Responsive card blog Prod

--- Portfolio/src/components/magicui/magic-card-5-6.tsx
+++ Portfolio/src/components/magicui/magic-card-5-6.tsx
@@ -12,7 +12,7 @@ export function MagicCard5_6() {
         >
             <div className="w-screen h-screen flex flex-col items-center justify-around">
                 <MagicCard
-                    className="cursor-pointer w-1/3 h-2/5 bg-gray-950 text-white shadow-2xl p-4 flex flex-col justify-between"
+                    className="cursor-pointer w-4/5 h-2/5 bg-gray-950 text-white shadow-2xl p-4 flex flex-col justify-between md:w-1/3 md:h-2/5"
                     gradientColor={theme === "dark" ? "rgba(124,215,233,0.74)" : "rgba(9,94,220,0.7)"}
                 >
                     <div className="w-full flex justify-between">
@@ -34,10 +34,25 @@ export function MagicCard5_6() {
                 </MagicCard>
 
                 <MagicCard
-                    className="cursor-pointer w-1/3 h-2/5 bg-gray-950 text-white flex-col items-center justify-center shadow-2xl whitespace-nowrap text-4xl"
+                    className="cursor-pointer w-4/5 h-2/5 bg-gray-950 text-white shadow-2xl p-4 flex flex-col justify-between md:w-1/3 md:h-2/5"
                     gradientColor={theme === "dark" ? "rgba(124,215,233,0.74)" : "rgba(9,94,220,0.7)"}
                 >
-                    Card
+                    <div className="w-full flex justify-between">
+                        {/* Date */}
+                        <p className="text-gray-400 text-sm">27/09/24</p>
+
+                        {/* Nom */}
+                        <p className="text-gray-400 text-sm">Théo CERKOWNIK</p>
+                    </div>
+
+
+                    {/* Title */}
+                    <h2 className="text-xl font-bold text-white">Mon stage chez Docaposte</h2>
+
+                    {/* Excerpt */}
+                    <p className="text-sm text-gray-300">
+                        J'ai découvert le développement Java, la sécurité SOC, et la maintenance/infra. Une expérience enrichissante...
+                    </p>
                 </MagicCard>
             </div>
         </div>
