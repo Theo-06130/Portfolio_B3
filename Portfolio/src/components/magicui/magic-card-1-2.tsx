aff7c16c8b1a14f5c19c00d4fd4a1cc4bb505fce
Theo-06130 <theo.cerkownik@ynov.com>
2024-09-28 12:00:36 +0200

    Changement taille card

--- Portfolio/src/components/magicui/magic-card-1-2.tsx
+++ Portfolio/src/components/magicui/magic-card-1-2.tsx
@@ -11,7 +11,7 @@ export function MagicCard1_2() {
             <div className="w-screen h-screen flex flex-col items-center justify-around">
                 {/* Première MagicCard */}
                 <MagicCard
-                    className="cursor-pointer w-screen m-6 h-1/2 bg-gray-950 text-white shadow-2xl p-4 flex flex-col justify-between md:w-1/3 md:h-2/5"
+                    className="cursor-pointer w-[calc(100%-24px)] h-[calc(50%-12px)] bg-gray-950 text-white shadow-2xl p-4 flex flex-col justify-between md:w-1/3 md:h-2/5"
                     gradientColor={theme === "dark" ? "rgba(124,215,233,0.74)" : "rgba(9,94,220,0.7)"}
                 >
                     <div className="w-full flex justify-between">
@@ -33,7 +33,7 @@ export function MagicCard1_2() {
 
                 {/* Deuxième MagicCard */}
                 <MagicCard
-                    className="cursor-pointer w-4/5 h-2/5 bg-gray-950 text-white shadow-2xl p-4 flex flex-col justify-between md:w-1/3 md:h-2/5"
+                    className="cursor-pointer w-[calc(100%-24px)] h-[calc(50%-12px)] bg-gray-950 text-white shadow-2xl p-4 flex flex-col justify-between md:w-1/3 md:h-2/5"
                     gradientColor={theme === "dark" ? "rgba(124,215,233,0.74)" : "rgba(9,94,220,0.7)"}
                 >
                     <div className="w-full flex justify-between">
