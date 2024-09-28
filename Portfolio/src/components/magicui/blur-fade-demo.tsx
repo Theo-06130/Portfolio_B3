3aa5357130f70c5ebe452fca24090448bd9785a1
Theo-06130 <theo.cerkownik@ynov.com>
2024-09-28 14:49:50 +0200

    Ajout projet

--- Portfolio/src/components/magicui/blur-fade-demo.tsx
+++ Portfolio/src/components/magicui/blur-fade-demo.tsx
@@ -1,33 +1,37 @@
 import BlurFade from "@/components/magicui/blur-fade";
 import React from "react";
+import biblivres from '../../assets/biblivres.png'
+import accessibilite from '../../assets/bouche_glacee.png'
+import spoonacular from '../../assets/spoonacular.png'
+
 
 const images = [
     {
-        url: 'https://picsum.photos/seed/1/800/600',
-        link: 'https://github.com/Theo-06130/Portfolio_B3'  // Remplace par ton dépôt GitHub
+        src: biblivres, // Chemin vers l'image locale
+        link: 'https://biblivres.sqwado.com/home',  // Lien associé
     },
     {
-        url: 'https://picsum.photos/seed/2/600/800',
-        link: 'https://github.com/Theo-06130'
+        src: accessibilite,
+        link: 'https://theo-06130.github.io/Site_accessibilite/',
     },
     {
-        url: 'https://picsum.photos/seed/3/800/600',
-        link: 'https://github.com/Theo-06130/spoonacular'
+        src: spoonacular,
+        link: 'https://theo-06130.github.io/spoonacular/',
     },
-    // Ajoute d'autres objets comme nécessaire
+    // Ajoute d'autres objets si nécessaire
 ];
 
 export function BlurFadeDemo() {
     return (
         <section id="photos" className="h-screen mt-16 overflow-auto">
             <div className="columns-2 gap-4 sm:columns-3">
-                {images.map(({ url, link }, idx) => (
-                    <a href={link} key={url} target="_blank" rel="noopener noreferrer">
+                {images.map(({ src, link }, idx) => (
+                    <a href={link} key={src} target="_blank" rel="noopener noreferrer">
                         <BlurFade delay={0.25 + idx * 0.05} inView>
                             <img
                                 className="mb-4 w-full h-auto max-h-[20vh] rounded-lg object-contain"
-                                src={url}
-                                alt={`Random stock image ${idx + 1}`}
+                                src={src}  // Utilisez src ici
+                                alt={`Image ${idx + 1}`}
                             />
                         </BlurFade>
                     </a>
